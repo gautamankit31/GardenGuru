a549e81eb69f6465a72fbaeda76aa39e6d664741
Ankit Raj Mishra <ankitmishra96889@gmail.com>
2025-04-26 14:45:32 +0530

    changes

--- frontend/src/components/core/Auth/Template.jsx
+++ frontend/src/components/core/Auth/Template.jsx
@@ -7,39 +7,21 @@ const Template = ({heading , subheading, image, specialText, page}) => {
     <img
       src={image[0]}
       alt="mage"
-      className="absolute inset-0 w-full max-w-[300px] md:max-w-md blur-sm"
+      className="absolute inset-0 max-h-[600px]  max-w-[400px] md:max-w-md blur-sm"
     />
     <img
       src={image[1]}
       alt="mage"
-      className="relative z-10 w-full max-w-[300px] md:max-w-md ml-14 py-[50px]"
+      className="relative z-10 w-[500px] max-h-[600px] max-w-[400px] md:max-w-md ml-14 py-[50px]"
     />
   </div>
-  <div className="flex flex-col justify-center">
+  <div className="flex flex-col justify-center md:ml-[20px]">
     <h2 className="text-3xl font-semibold mb-5">Let's Get Started.</h2>
     {
         page === 'login' ? (<LoginForm></LoginForm>) : (<SignupForm></SignupForm>) 
     }
   </div>
   </>
-    // <div className='mx-auto flex w-11/12 max-w-maxContent flex-col-reverse justify-between gap-y-12 py-12 md:flex-row md:gap-y-0 md:gap-x-12'>
-    //   <div className='mx-auto w-11/12 max-w-[450px] md:mx-0'>
-    //     <h1 className='text-[1.875rem] font-semibold leading-[2.375rem] text-richblack-5 mt-5'>{heading}</h1>
-    //     <p className='text-richblack-100 mt-5'>{subheading} <span className='font-edu-sa font-bold italic text-blue-100'>{specialText}</span></p>
-    //     <div className='mt-5'>
-    //       {
-    //         page === 'login' ? (<LoginForm></LoginForm>) : (<SignupForm></SignupForm>) 
-    //       }
-    //     </div>
-    //   </div>
-      
-    //   <div>
-    //     <div className='relative mx-auto w-11/12 max-w-[550px] md:mx-0'>
-    //       <img src={image} alt={page} className='absolute -top-4 right-4 z-10'/>
-    //       <img src={frame} alt="" />
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,9 +7,6 @@ const Login = () => {
     <div className="w-full min-h-screen md:bg-[#c4d7cf] py-16">
       <div className="bg-white md:max-w-[1280px] mx-auto grid md:gap-y-10 md:grid-cols-2 max-w-[600px] px-4 rounded-3xl shadow-xl">
         <Template
-            // heading = {"Welcome Back"}
-            // subheading = {"Build skills for today, tomorrow, and beyond."}
-            // specialText = {"Education to future-proof your career."}
             image={[login1,login2]}
             page={"login"}
         ></Template>
